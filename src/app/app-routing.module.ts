882c82d88d1da99e72a17f6a0e493226ab7178c6
wahib bachoua <wahibbachoua95@gmail.com>
2025-05-24 20:15:00 +0100

    Refactor routing configuration for improved clarity; update header component to use routerLink for navigation; enhance movie list component with service integration for fetching movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,15 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'movies-list', component: MovieListComponent },
   { path: 'movie-details/:id', component: MovieDetailComponent },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Film} from '../models/film';
+import { AppService } from '../sevices/app.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,4 +11,19 @@ import { Film} from '../models/film';
 })
 export class MovieListComponent {
   films : Film[] =[]; 
+  constructor(private router: Router ,private appService: AppService ) {}
+
+  getallMovies() {
+    // Call the service to get all movies
+     this.appService.getAllMovies().subscribe(
+      (Data: any) => {
+        this.films = Data;
+        console.log('Films:', this.films);
+      },
+    );
+  }
+  ngOnInit() {
+    // Call the service to get all movies
+    this.getallMovies();
+  }
 }
